49a62e5aa5f4f1f44f2a303425025d39edffb79a
DeniSaya <deny.pop06@gmail.com>
2016-08-03 17:21:52 +0300

    added ify

--- transform/script.js
+++ transform/script.js
@@ -1,12 +1,13 @@
-function checkCase(letter) {
-    var upper = letter.toUpperCase();
-    if ( letter == upper ) {
-        upper = letter.toLowerCase();
-    } return upper;
-}
-
+(function () {
+    function checkCase(letter) {
+        var upper = letter.toUpperCase();
+        if ( letter == upper ) {
+            upper = letter.toLowerCase();
+        } return upper;
+    }
+})();
 function alterCase() {
-    var input = document.querySelector("#text")
+    var input = document.querySelector("#text");
     var str = input.value;
     var result = "";
     for (var i = 0; i < str.length; i++) {
