926163db9f711d15bc2fd97d6097d1770a2b53e0
DeniSaya <deny.pop06@gmail.com>
2016-08-17 14:32:09 +0300

    remove hide inputs

--- app/js/user-list.js
+++ app/js/user-list.js
@@ -8,10 +8,8 @@ var listOfUsers = (function() {
             showUsers += [
                 '<li>',
                     '<h4>' + users[i].name + '</h4>',
-                    '<input type="hidden" class="js-edited-name">',
                     '<p>' + users[i].description + '</p>',
-                    '<input type="hidden" class="js-edited-description">',
-                    //'<button class="save" onclick="app.saveUser(' + users[i].id + ');"> Save </button>',
+                    '<button class="save" onclick="app.saveUser(' + users[i].id + ');"> Save </button>',
                     '<button onclick="app.editUser(' + users[i].id + ');"> Edit </button>',
                     '<button onclick="app.deleteUser(' + users[i].id + ');"> Delete </button>',
                 '</li>'
@@ -28,11 +26,9 @@ var listOfUsers = (function() {
     ListOfUsers.editUser = function(id) {
         for (var i = 0; i < Users.length; i++) {
             var obj = Users[i];
-            var showName = document.querySelector('.js-edited-name');
-            var showDescription = document.querySelector('.js-edited-description');
+            console.log(obj);
             if (obj.id == id) {
-                showName.type="text";
-                showDescription.type="text";
+                onj.id = obj.name;
             }
         }
     }
